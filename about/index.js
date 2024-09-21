25f75567cecfe845d7e3fb3fe913252f58a3649a
Hukeqing <Hukeqing@users.noreply.github.com>
2024-09-21 08:44:26 +0000

    deploy: 946d00e53e82a91cb8d01e730ac30c06e32dabb9

--- about/index.js
+++ about/index.js
@@ -1,282 +1,218 @@
-const scrollObjects = [
-    {
-        animate: [
-            {
-                top: '50vh',
-                left: '50vw',
-                width: '30vh',
-                height: '30vh',
-                offset: 0
-            },
-            {
-                top: '20vh',
-                left: '50vw',
-                width: '20vh',
-                height: '20vh',
-                offset: 1
-            },
-            {
-                top: '20vh',
-                left: '50vw',
-                width: '20vh',
-                height: '20vh',
-                offset: 3
-            },
-            {
-                top: '40vh',
-                left: '20vw',
-                width: '20vh',
-                height: '20vh',
-                offset: 4
-            },
-        ],
-        domId: "avatar"
-    },
-    {
-        animate: [
-            {
-                opacity: 0,
-                pointerEvents: 'none',
-                width: '50vh',
-                left: '-15vh',
-                fontSize: '2rem',
-                fontWeight: '900',
-                offset: 0
-            },
-            {
-                opacity: 0,
-                pointerEvents: 'none',
-                width: '50vh',
-                left: '-15vh',
-                fontSize: '2rem',
-                fontWeight: '900',
-                offset: 1
-            },
-            {
-                opacity: 0,
-                pointerEvents: 'none',
-                width: '50vh',
-                left: '-15vh',
-                fontSize: '2rem',
-                fontWeight: '900',
-                offset: 3
-            },
-            {
-                opacity: 1,
-                pointerEvents: 'auto',
-                width: '50vh',
-                left: '-15vh',
-                fontSize: '2rem',
-                fontWeight: '900',
-                offset: 4
-            },
-        ],
-        domId: "avatar-name",
-    },
-    {
-        animate: [
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 0
-            },
-            {
-                bottom: '50vh',
-                opacity: 1,
-                fontSize: '3rem',
-                offset: 1
-            },
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 2
-            },
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 4
-            },
-        ],
-        domId: "home-title-1"
-    },
-    {
-        animate: [
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 0
-            },
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 1
-            },
-            {
-                bottom: '50vh',
-                opacity: 1,
-                fontSize: '3rem',
-                offset: 2
-            },
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 3
-            },
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 4
-            }
-        ],
-        domId: "home-title-2"
-    },
-    {
-        animate: [
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 0
-            },
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 1
-            },
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 2
-            },
-            {
-                bottom: '50vh',
-                opacity: 1,
-                fontSize: '3rem',
-                offset: 3
-            },
-            {
-                bottom: '10vh',
-                opacity: 0,
-                fontSize: '1rem',
-                offset: 4
-            }
-        ],
-        domId: "home-title-3"
-    },
-    {
-        animate: [
-            {
-                paddingLeft: '10vw',
-                opacity: 0,
-                offset: 0
-            },
-            {
-                paddingLeft: '10vw',
-                opacity: 0,
-                offset: 3
-            },
-            {
-                paddingLeft: '0',
-                opacity: 1,
-                offset: 4
-            },
-        ],
-        domId: "intro-1",
-    },
-    {
-        animate: [
-            {
-                paddingLeft: '13vw',
-                opacity: 0,
-                offset: 0
-            },
-            {
-                paddingLeft: '13vw',
-                opacity: 0,
-                offset: 3
-            },
-            {
-                paddingLeft: '0',
-                opacity: 1,
-                offset: 4
-            },
-        ],
-        domId: "intro-2",
-    },
-    {
-        animate: [
-            {
-                paddingLeft: '16vw',
-                opacity: 0,
-                offset: 0
-            },
-            {
-                paddingLeft: '16vw',
-                opacity: 0,
-                offset: 3
-            },
-            {
-                paddingLeft: '0',
-                opacity: 1,
-                offset: 4
-            },
-        ],
-        domId: "intro-3",
-    },
-    {
-        animate: [
-            {
-                paddingLeft: '19vw',
-                opacity: 0,
-                offset: 0
-            },
-            {
-                paddingLeft: '19vw',
-                opacity: 0,
-                offset: 3
-            },
-            {
-                paddingLeft: '0',
-                opacity: 1,
-                offset: 4
-            },
-        ],
-        domId: "intro-4",
-    },
-    {
-        animate: [
-            {
-                paddingLeft: '22vw',
-                opacity: 0,
-                offset: 0
-            },
-            {
-                paddingLeft: '22vw',
-                opacity: 0,
-                offset: 3
-            },
-            {
-                paddingLeft: '0',
-                opacity: 1,
-                offset: 4
-            },
-        ],
-        domId: "intro-5",
-    },
-]
+const scrollObjects = [{
+    domId: "home",
+    animateList: [
+        {
+            animate: [
+                {
+                    top: '50vh',
+                    left: '50vw',
+                    width: '30vh',
+                    height: '30vh',
+                    offset: 0
+                },
+                {
+                    top: '20vh',
+                    left: '50vw',
+                    width: '20vh',
+                    height: '20vh',
+                    offset: 1
+                },
+                {
+                    top: '20vh',
+                    left: '50vw',
+                    width: '20vh',
+                    height: '20vh',
+                    offset: 3
+                },
+                {
+                    top: '40vh',
+                    left: '20vw',
+                    width: '20vh',
+                    height: '20vh',
+                    offset: 4
+                },
+            ],
+            domId: "avatar"
+        },
+        {
+            animate: [
+                {
+                    opacity: 0,
+                    pointerEvents: 'none',
+                    width: '50vh',
+                    left: '-15vh',
+                    fontSize: '2rem',
+                    fontWeight: '900',
+                    offset: 3
+                },
+                {
+                    opacity: 1,
+                    pointerEvents: 'auto',
+                    width: '50vh',
+                    left: '-15vh',
+                    fontSize: '2rem',
+                    fontWeight: '900',
+                    offset: 4
+                },
+            ],
+            domId: "avatar-name",
+        },
+        {
+            animate: [
+                {
+                    bottom: '10vh',
+                    opacity: 0,
+                    fontSize: '1rem',
+                    offset: 0
+                },
+                {
+                    bottom: '50vh',
+                    opacity: 1,
+                    fontSize: '3rem',
+                    offset: 1
+                },
+                {
+                    bottom: '10vh',
+                    opacity: 0,
+                    fontSize: '1rem',
+                    offset: 2
+                }
+            ],
+            domId: "home-title-1"
+        },
+        {
+            animate: [
+                {
+                    bottom: '10vh',
+                    opacity: 0,
+                    fontSize: '1rem',
+                    offset: 1
+                },
+                {
+                    bottom: '50vh',
+                    opacity: 1,
+                    fontSize: '3rem',
+                    offset: 2
+                },
+                {
+                    bottom: '10vh',
+                    opacity: 0,
+                    fontSize: '1rem',
+                    offset: 3
+                }
+            ],
+            domId: "home-title-2"
+        },
+        {
+            animate: [
+                {
+                    bottom: '10vh',
+                    opacity: 0,
+                    fontSize: '1rem',
+                    offset: 2
+                },
+                {
+                    bottom: '50vh',
+                    opacity: 1,
+                    fontSize: '3rem',
+                    offset: 3
+                },
+                {
+                    bottom: '10vh',
+                    opacity: 0,
+                    fontSize: '1rem',
+                    offset: 4
+                }
+            ],
+            domId: "home-title-3"
+        },
+        {
+            animate: [
+                {
+                    paddingLeft: '10vw',
+                    opacity: 0,
+                    offset: 3
+                },
+                {
+                    paddingLeft: '0',
+                    opacity: 1,
+                    offset: 4
+                },
+            ],
+            domId: "intro-1",
+        },
+        {
+            animate: [
+                {
+                    paddingLeft: '13vw',
+                    opacity: 0,
+                    offset: 3
+                },
+                {
+                    paddingLeft: '0',
+                    opacity: 1,
+                    offset: 4
+                },
+            ],
+            domId: "intro-2",
+        },
+        {
+            animate: [
+                {
+                    paddingLeft: '16vw',
+                    opacity: 0,
+                    offset: 3
+                },
+                {
+                    paddingLeft: '0',
+                    opacity: 1,
+                    offset: 4
+                },
+            ],
+            domId: "intro-3",
+        },
+        {
+            animate: [
+                {
+                    paddingLeft: '19vw',
+                    opacity: 0,
+                    offset: 3
+                },
+                {
+                    paddingLeft: '0',
+                    opacity: 1,
+                    offset: 4
+                },
+            ],
+            domId: "intro-4",
+        },
+        {
+            animate: [
+                {
+                    paddingLeft: '22vw',
+                    opacity: 0,
+                    offset: 3
+                },
+                {
+                    paddingLeft: '0',
+                    opacity: 1,
+                    offset: 4
+                },
+            ],
+            domId: "intro-5",
+        },
+    ],
+    length: 4
+}, {
+    domId: "project",
+    animateList: [],
+    length: 1
+}]
 
 const totalFrame = 10000
 const animateList = []
 const playTime = 1
+let bg = null
 let maxLength = 0
 let onScroll = false
 let from = 0
@@ -284,31 +220,40 @@ let to = 0
 let frame = 0
 
 const openUrl = (url) => {
-    // console.log(url)
     window.open(url, '_blank')
 }
 
 const start = () => {
-    for (const animate of scrollObjects) for (const x of animate.animate)
-        maxLength = Math.max(maxLength, x.offset)
-
+    bg = document.getElementById("bg");
     for (const animate of scrollObjects) {
-        for (const x of animate.animate) x.offset /= 4
-        const child = document.getElementById(animate.domId)
-        const animation = child.animate(animate.animate, totalFrame)
-        animation.pause()
-        animateList.push({
-            animation: animation,
-            startPos: 0,
-            endPos: maxLength
-        })
+        maxLength += animate.length
+        const container = document.getElementById(animate.domId)
+        container.style.height = (animate.length + 1) + '00vh'
+        for (const item of animate.animateList) {
+            let start = 1000000, end = 0
+            for (const x of item.animate) {
+                end = Math.max(end, x.offset)
+                start = Math.min(start, x.offset)
+            }
+            for (const x of item.animate) x.offset = (x.offset - start) / (end - start)
+            const child = document.getElementById(item.domId)
+            const animation = child.animate(item.animate, totalFrame)
+            animation.currentTime = 0
+            animation.pause()
+            animateList.push({
+                animation: animation,
+                start: start,
+                length: end - start
+            })
+        }
     }
 }
 
-const play = (animation, process) => {
-    animation.currentTime = (() => {
-        return clamp(process * totalFrame, 0, totalFrame - 1)
-    })()
+const play = (process) => {
+    for (const item of animateList) {
+        item.animation.currentTime = clamp((process - item.start) / item.length * totalFrame, 0, totalFrame - 1)
+    }
+    window.scrollTo({ top: bg.offsetHeight * process })
 }
 
 const scroll = (event) => {
@@ -328,9 +273,7 @@ const clampPlay = () => {
     const f = frame / 100
     const tmp = 1 - Math.pow(1 - f, 4)
     const value = from * (1 - tmp) + to * tmp
-    for (const item of animateList) {
-        play(item.animation, (value - item.startPos) / (item.endPos - item.startPos))
-    }
+    play(value)
 }
 
 const clamp = (v, l, r) => {
